3aab3ee7ca6745e9c4dc3c9b171fd22e57b73bcd
skyLiyx <ysing321@outlook.com>
2023-09-06 20:34:08 +0800

    add some implementations that solve some backtracking issues.

---
+++ src/com/lyx/leetcode/Lc0046.java
@@ -0,0 +1,38 @@
+package com.lyx.leetcode;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * 47.全排列
+ *
+ * 思路：回溯
+ *
+ * @version 2023/09/06
+ */
+public class Lc0046 {
+    public List<List<Integer>> permute(int[] nums) {
+        List<List<Integer>> res = new ArrayList<>();
+        boolean[] visited = new boolean[nums.length];
+        backtrace(nums, visited, new ArrayList<>(), res);
+        return res;
+    }
+
+    private void backtrace(int[] nums, boolean[] visited, List<Integer> list, List<List<Integer>> res) {
+        if (list.size() == nums.length) {
+            res.add(new ArrayList<>(list));
+            return;
+        }
+        for (int i = 0; i < nums.length; i++) {
+            if (visited[i]) {
+                continue;
+            }
+            visited[i] = true;
+            list.add(nums[i]);
+            backtrace(nums, visited, list, res);
+            list.remove(list.size() - 1);
+            visited[i] = false;
+        }
+    }
+
+}
