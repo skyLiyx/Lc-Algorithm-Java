c8d6b9008e763ba175d440ece31291eb3b447948
skyLiyx <ysing321@outlook.com>
2023-10-24 21:38:22 +0800

    update a better solution of No.337.

--- src/com/lyx/leetcode/c3/Lc0337.java
+++ src/com/lyx/leetcode/c3/Lc0337.java
@@ -16,8 +16,12 @@ public class Lc0337 {
     private final Map<TreeNode, Integer> rob = new HashMap<>();
     private final Map<TreeNode, Integer> notRob = new HashMap<>();
     public int rob(TreeNode root) {
-        dfs(root);
-        return Math.max(rob.get(root), notRob.get(root));
+        // 方式一
+        // dfs(root);
+        // return Math.max(rob.get(root), notRob.get(root));
+        // 方式二
+        Result result = process(root);
+        return Math.max(result.rob, result.notRob);
     }
 
     private void dfs(TreeNode root) {
@@ -34,5 +38,32 @@ public class Lc0337 {
         rob.put(root, root.val + notRobLeft + notRobRight);
         // 记录不选择当前节点时的最大值：选择或不选择左孩子的最大值 + 选择或不选择右孩子的最大值
         notRob.put(root, Math.max(robLeft, notRobLeft) + Math.max(robRight, notRobRight));
+    }
+
+
+
+    private Result process(TreeNode node) {
+        if (node == null) {
+            return new Result(0, 0);
+        }
+        // 左孩子的结果
+        Result leftResult = process(node.left);
+        // 右孩子的结果
+        Result rightResult = process(node.right);
+        // 取当前节点=左孩子不取+右孩子不取+当前节点的值
+        int rob = leftResult.notRob + rightResult.notRob + node.val;
+        // 不取当前节点=左孩子取或不取中的较大值+右孩子取或不取中的较大值
+        int notRob = Math.max(leftResult.rob, leftResult.notRob) + Math.max(rightResult.rob, rightResult.notRob);
+        return new Result(rob, notRob);
+    }
+
+    private static class Result {
+        int rob;
+        int notRob;
+
+        public Result(int rob, int notRob) {
+            this.rob = rob;
+            this.notRob = notRob;
+        }
     }
 }
